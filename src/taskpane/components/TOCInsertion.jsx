61c825825714747dde5045f10f61f33a2bcfdb98
jackhaley <jackmhaley@gmail.com>
2024-09-11 19:48:51 -0400

    Insert TOC working

---
+++ src/taskpane/components/TOCInsertion.jsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { useState } from "react";
+import { Button, Field, Textarea, tokens, makeStyles } from "@fluentui/react-components";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles({
+  instructions: {
+    fontWeight: tokens.fontWeightSemibold,
+    marginTop: "20px",
+    marginBottom: "10px",
+  },
+  textPromptAndInsertion: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  textAreaField: {
+    marginLeft: "20px",
+    marginTop: "30px",
+    marginBottom: "20px",
+    marginRight: "20px",
+    maxWidth: "50%",
+  },
+});
+
+const TOCInsertion = (props) => {
+
+  const handleTocInsertion = async () => {
+    console.log('this is thehandle');
+    
+    if (typeof props.insertToc === "function") {
+      console.log("insertToc is a valid function");
+      await props.insertToc();
+    } else {
+      console.log("insertToc is not a function or is undefined");
+    }
+    
+    await props.insertToc;
+  };
+
+  const styles = useStyles();
+
+  return (
+    <div className={styles.textPromptAndInsertion}>
+      <Button appearance="primary" disabled={false} size="large" onClick={handleTocInsertion}>
+        Insert Table of Contents
+      </Button>
+    </div>
+  );
+};
+
+export default TOCInsertion;
